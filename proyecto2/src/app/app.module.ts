6c643a169da7ac8b18f7896e78880ce36765878a
dannytorresuta <86840935+dannytorresuta@users.noreply.github.com>
2021-11-07 12:58:16 -0300

    10 cambios

--- proyecto2/src/app/app.module.ts
+++ proyecto2/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicTableComponent } from './basic-table/basic-table.component';
+import { BlankComponent } from './blank/blank.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicTableComponent,
+    BlankComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
