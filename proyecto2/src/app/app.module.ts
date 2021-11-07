73ed5d02498c3dab0374f7a0eb7980a7af469633
dannytorresuta <86840935+dannytorresuta@users.noreply.github.com>
2021-11-07 19:25:19 -0300

    eliminando componentes

--- proyecto2/src/app/app.module.ts
+++ proyecto2/src/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppComponent } from './app.component';
 import { BasicTableComponent } from './basic-table/basic-table.component';
 import { BlankComponent } from './blank/blank.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { FontawesomeComponent } from './fontawesome/fontawesome.component';
-import { MapGoogleComponent } from './map-google/map-google.component';
-import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -15,9 +12,6 @@ import { ProfileComponent } from './profile/profile.component';
     BasicTableComponent,
     BlankComponent,
     DashboardComponent,
-    FontawesomeComponent,
-    MapGoogleComponent,
-    ProfileComponent
   ],
   imports: [
     BrowserModule
