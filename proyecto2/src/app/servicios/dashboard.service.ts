0b9d850a30fe33264f2bbe65b34dd4234ab32468
dannytorresuta <86840935+dannytorresuta@users.noreply.github.com>
2021-11-09 13:46:06 -0300

    agregando servicios al dashboard

--- proyecto2/src/app/servicios/dashboard.service.ts
+++ proyecto2/src/app/servicios/dashboard.service.ts
@@ -24,6 +24,30 @@ export class DashboardService {
             estado: "EXTENDED",
             fecha: "April 19, 2021",
             precio: 20
+        },
+        {
+            nombre: "Medical Pro WP Theme",
+            estado: "TAX",
+            fecha: "April 20, 2021",
+            precio: -24
+        },
+        {
+            nombre: "Hosting press html",
+            estado: "SALE",
+            fecha: "April 21, 2021",
+            precio: 24
+        },
+        {
+            nombre: "Digital Agency PSD",
+            estado: "SALE",
+            fecha: "April 23, 2021",
+            precio: -14
+        },
+        {
+            nombre: "Helping Hands WP Theme",
+            estado: "MEMBER",
+            fecha: "April 22, 2021",
+            precio: 64
         }
     ];
 
